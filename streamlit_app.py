43c27468066ca1dba4bfde322e325a8b2e44755d
carter-lowe-cs <carter.lowe@cellularsales.com>
2025-12-03 10:34:27 -0500

    Update streamlit_app.py
    Added DF for smoothiefroot nutrition

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -44,3 +43,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
+# New section to display smoothiefroot nutrition information
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data = smoothiefroot_response.json, use_container_width = True)
